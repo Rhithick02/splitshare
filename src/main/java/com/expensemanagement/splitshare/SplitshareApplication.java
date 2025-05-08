aa83158c7c271a5626839f4bb72ca255f6b3e63b
rm002 <rhithick_m@intuit.com>
2025-05-08 21:51:25 +0530

    first commit

---
+++ src/main/java/com/expensemanagement/splitshare/SplitshareApplication.java
@@ -0,0 +1,13 @@
+package com.expensemanagement.splitshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitshareApplication.class, args);
+    }
+
+}

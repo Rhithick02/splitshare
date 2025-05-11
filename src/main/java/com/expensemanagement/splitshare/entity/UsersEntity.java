d461a98cd55cf584239b43410c840c25ec69ee25
Shreya <shreya_mour@intuit.com>
2025-05-11 22:23:46 +0530

    Creating entities

---
+++ src/main/java/com/expensemanagement/splitshare/entity/UsersEntity.java
@@ -0,0 +1,115 @@
+package com.expensemanagement.splitshare.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class UsersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long userId;
+
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private String countryCode;
+    private Timestamp createDate;
+    private Timestamp updateDate;
+
+    @ManyToMany
+    @JoinTable( name = "user_groups",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "group_id"))
+    private Set<GroupsEntity> groups;
+
+    @PrePersist
+    public void setCreationAndUpdationDate() {
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        this.createDate = timestamp;
+        this.updateDate = timestamp;
+    }
+
+    @PreUpdate
+    public void setUpdationDate() {
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        this.updateDate = timestamp;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Set<GroupsEntity> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Set<GroupsEntity> groups) {
+        this.groups = groups;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+    }
+}

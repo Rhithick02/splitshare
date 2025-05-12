46489809aaffa60a38eea113e35cde73650b8401
Rhithick <rhithick02@gmail.com>
2025-05-12 23:46:49 +0530

    payment and transaction entity creation

--- src/main/java/com/expensemanagement/splitshare/entity/TransactionsEntity.java
+++ src/main/java/com/expensemanagement/splitshare/entity/TransactionsEntity.java
@@ -1,69 +1,114 @@
-//package com.expensemanagement.splitshare.entity;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import jakarta.persistence.Table;
-//
-//@Entity
-//@Table(name = "transactions")
-//public class TransactionsEntity {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//    private Long transactionId;
-//
-//    private Long groupId;
-//    private Double totalAmount;
-//    private String method;
-//    private Long createdBy;
-//    private Long updatedBy;
-//
-//    public Long getTransactionId() {
-//        return transactionId;
-//    }
-//
-//    public void setTransactionId(Long transactionId) {
-//        this.transactionId = transactionId;
-//    }
-//
-//    public Long getGroupId() {
-//        return groupId;
-//    }
-//
-//    public void setGroupId(Long groupId) {
-//        this.groupId = groupId;
-//    }
-//
-//    public Double getTotalAmount() {
-//        return totalAmount;
-//    }
-//
-//    public void setTotalAmount(Double totalAmount) {
-//        this.totalAmount = totalAmount;
-//    }
-//
-//    public String getMethod() {
-//        return method;
-//    }
-//
-//    public void setMethod(String method) {
-//        this.method = method;
-//    }
-//
-//    public Long getCreatedBy() {
-//        return createdBy;
-//    }
-//
-//    public void setCreatedBy(Long createdBy) {
-//        this.createdBy = createdBy;
-//    }
-//
-//    public Long getUpdatedBy() {
-//        return updatedBy;
-//    }
-//
-//    public void setUpdatedBy(Long updatedBy) {
-//        this.updatedBy = updatedBy;
-//    }
-//}
+package com.expensemanagement.splitshare.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Entity
+@Table(name = "transactions")
+public class TransactionsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long transactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private GroupsEntity group;
+    private Double totalAmount;
+    private String method;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by")
+    private UsersEntity createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_by")
+    private UsersEntity updatedBy;
+    private Timestamp createDate;
+    private Timestamp updateDate;
+
+    @PrePersist
+    public void setCreationAndUpdationDate() {
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        this.createDate = timestamp;
+        this.updateDate = timestamp;
+    }
+
+    @PreUpdate
+    public void setUpdationDate() {
+        Timestamp timestamp = new Timestamp(new Date().getTime());
+        this.updateDate = timestamp;
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public GroupsEntity getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupsEntity group) {
+        this.group = group;
+    }
+
+    public UsersEntity getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UsersEntity createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public UsersEntity getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(UsersEntity updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+    }
+}
